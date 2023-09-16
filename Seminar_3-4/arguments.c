310a5334a7655a5b645f40843e18dbe73ebb6f3c
IlyaID <ivanov.id@phystech.edu>
2023-09-16 10:04:33 +0300

    upd

---
+++ Seminar_3-4/arguments.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+    for(int i = 0; i < sizeof(argc); i++){
+        printf("Argc%d = %s\n", i, argv[i]);
+    }
+
+    for(int i = 10; i > 0; i--){
+        printf("envp%d = %s\n", i, envp[i]);
+    }
+
+    if(argc == 1) {
+        printf("Command line has no additional arguments\n");
+    }
+    return 0;
+
+}
