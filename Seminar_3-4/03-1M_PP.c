08482810e455768e53890fdce64bf45732632935
IlyaID <ivanov.id@phystech.edu>
2023-09-16 10:12:41 +0300

    Update

--- Seminar_3-4/03-1M_PP.c
+++ Seminar_3-4/03-1M_PP.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[], char *envp[])
 {
     pid_t pid, ppid, chpid;
     int a = 0;
@@ -17,6 +17,12 @@ int main()
         ppid = getppid();
         a = a+1;
         printf("Hello mother! My pid = %d, my ppid = %d, result = %d\n", (int)pid, (int)ppid, a);
+        
+        (void) execle("/bin/cat", "/bin/cat", "arguments.c", 0, envp);
+        printf("Error on program start\n");
+        exit(-1);
+
+    return 0;
 
     } else {
         pid  = getpid();
@@ -25,4 +31,6 @@ int main()
         printf("Hello daughter! My pid = %d, my ppid = %d, result = %d\n", (int)pid, (int)ppid, a);
 
     }
+
+}
     
